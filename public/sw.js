bf2036b53bd7fd2944273ad306dfcb02bb9221f1
Anil Seervi <anil13112000@gmail.com>
2021-06-27 09:28:25 +0530

    Update cache version 🙂

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "site-static-v7";
-const dynamicCacheName = "site-dynamic-v7";
+const staticCacheName = "site-static-v8";
+const dynamicCacheName = "site-dynamic-v8";
 const assets = [
   "/",
   "/index.html",
